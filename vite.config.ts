9ade3a373c6ef3db961180a575848081d35eee30
kishanpp <kish101993@gmail.com>
2026-06-18 17:47:13 +0530

    fix: fix nginx path, added docker config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 // import EnvironmentPlugin from "vite-plugin-environment";
 // import { env } from "process";
 
-export default defineConfig({
-  // plugins: [react(), EnvironmentPlugin("all")],
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "",
-  // define: {
-  //   envWithProcessPrefix: {
-  //     "process.env": `${JSON.stringify(env)}`,
-  //   },
-  // },
-  server: {
-    hmr: {
-      overlay: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    // plugins: [react(), EnvironmentPlugin("all")],
+    plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/",
+    // define: {
+    //   envWithProcessPrefix: {
+    //     "process.env": `${JSON.stringify(env)}`,
+    //   },
+    // },
+    server: {
+      hmr: {
+        overlay: true,
+      },
+      port: Number(env.VITE_PORT),
     },
-  },
-});
+    preview: {
+      port: Number(env.VITE_PORT),
+    },
+  });
+};
